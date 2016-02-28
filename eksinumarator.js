12f6713ffc7702c9fb1a708d01207a1c323d9fb7
Burak T. DOGAN <burakdogan@gmail.com>
2016-02-27 18:21:16 -0800

    v1.3.1

--- eksinumarator.js
+++ eksinumarator.js
@@ -1,61 +1,67 @@
-$("document").ready(function(){
+$("document").ready(function () {
 
 
+    chrome.storage.sync.get("girdiSayisi", function (items) {
+
+        var count = parseInt(items.girdiSayisi);
+
+        var page = ($(".pager").data("currentpage") ? $(".pager").data("currentpage") : 1),
+            once = (parseInt($(".showall").text()) ? parseInt($(".showall").text()) : 0),
+            tot = page * count,
+            start = (page * count) - count + 1,
+            ente = Number(start) + Number(once),
+            ente2 = Number(tot) + Number(once);
+
+        for (i = ente; i <= ente2; i++) {
+            var cont = i - (page * count) + count - 1 - once;
+            var content = $(".content")[cont];
+            content.innerHTML = i + ". " + content.innerHTML;
+        }
+    });
+
+    $("a[href*='imgur']").each(function () {
+        this.href = this.href.replace("imgur", "filmot");
+    });
 
 
-chrome.storage.sync.get("girdiSayisi", function (items) {
+    chrome.storage.sync.get("ackapas", function (items) {
+        var ackapaf = items.ackapas;
 
-    var count = parseInt(items.girdiSayisi);
+        if (ackapaf) {
 
-    var page = ($(".pager").data("currentpage") ? $(".pager").data("currentpage") : 1),
-        once = (parseInt($(".showall").text()) ? parseInt($(".showall").text()) : 0),
-        tot = page * count,
-        start = (page * count) - count + 1,
-        ente = Number(start) + Number(once),
-        ente2 = Number(tot) + Number(once);
-
-    for (i = ente; i <= ente2; i++) {
-        var cont = i - (page * count) + count - 1 - once;
-        var content = $(".content")[cont];
-        content.innerHTML = i + ". " + content.innerHTML;
-    }
-});
-
-$("a[href*='imgur']").each(function () {
-    this.href = this.href.replace("imgur", "filmot");
-});
+            chrome.storage.sync.get("zaman", function (items) {
+                var timer = parseInt(items.zaman);
 
 
-chrome.storage.sync.get("ackapas", function (items) {
-    var ackapaf = items.ackapas;
-
-    if (ackapaf) {
-
-        chrome.storage.sync.get("zaman", function (items) {
-            var timer = parseInt(items.zaman);
+                setInterval(function () {
+                    $('#feed-refresh-link')[0].click();
+                }, timer);
 
 
-            setInterval(function () {
-                $('#feed-refresh-link')[0].click();
-            }, timer);
+            });
+        }
+
+    });
+
+    $("body").css("background-color", "#e6e6e6");
+    $("#index-section").css("width", "350px");
+    $("#main").css("margin-left", "380px");
+    $("#content-body").css({
+        "width": "100%",
+        "margin-left": "0px",
+        "margin-right": "0px"
+    });
+
+    $("#aside").hide();
 
 
-        });
-    }
+    chrome.storage.sync.get("konulus", function (items) {
+        var konuluf = items.konulus;
 
-});
+        if (konuluf) {
+            $("#videos").hide();
+        }
 
-
-$(".twitter-info-card").hide();
-$(".ad-banner").hide();
-
-chrome.storage.sync.get("konulus", function (items) {
-    var konuluf = items.konulus;
-
-    if (konuluf) {
-        $("#videos").hide();
-    }
-
-});
+    });
 
 })
